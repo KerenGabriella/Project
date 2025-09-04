bd3b33a23e4773f9d1571906ba7c0832e19041ca
Keren Gabriella <kerengabriella123@gmail.com>
2025-09-04 10:39:04 +0700

    first commit

---
+++ AplikasiDataLayananBPJS/src/DataBPJS/koneksi.java
@@ -0,0 +1,31 @@
+package DataBPJS;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class koneksi {
+Connection connect;
+    Statement sttmnt;
+    ResultSet rslt;
+    private static Connection koneksi;  
+    public static Connection getKoneksi(){
+        try{
+            String url = "jdbc:mysql://localhost:3306/db_bpjs";
+            String user = "root";
+            String password = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            koneksi = DriverManager.getConnection(url,user,password);
+            System.out.println("Koneksi berhasil");
+            
+        }catch(Exception e){
+            System.out.println("Koneksi Gagal");
+        }return koneksi;
+    
+    }  
+    public static void main(String[] args) {
+        
+    }
+    
+}
